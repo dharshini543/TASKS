ffab49425aa60421fb986a65a9448b58a81e021d
DHARSHINI T R <dharshinitr1998@gmail.com>
2024-10-15 15:03:03 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 6, num2 = 8, num3 = 2, num4 = 2, num5 = 3, num6 = 3, num7 = 4, num8 = 4, num9 = 5, num10 = 5;
+    printf("%d\n %d\n %d\n %d\n %d\n",num1 + num2, num3 + num4, num5 + num6, num7 + num8, num9 + num10);
+    return 0;
+}
